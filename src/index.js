7e99e1c2135f2aeb25bdc8a9ecec06e64912f7db
Ilya Rodin <i1.rodin@yandex.ru>
2023-03-05 12:20:23 +0300

    change function argument name

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import greetUser from './cli.js';
 
-export default (callback, repeats) => {
+export default (playOneRound, repeats) => {
   const userName = greetUser();
   for (let i = 0; i < repeats; i += 1) {
-    const [userAnswer, correctAnswer] = callback(userName);
+    const [userAnswer, correctAnswer] = playOneRound(userName);
 
     if (userAnswer !== correctAnswer) {
       console.log(`Let's try again, ${userName}!`);
