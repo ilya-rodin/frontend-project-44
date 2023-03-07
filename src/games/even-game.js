a8d4e553558daf760fe479f9a6b8d45957f852b7
Ilya Rodin <i1.rodin@yadnex.ru>
2023-03-07 19:49:59 +0300

    eslint --fix execute

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 import generateNumber from '../utils.js';
 import playSeveralTimes from '../index.js';
 
-
 const playEven = () => {
   const number = generateNumber(100);
   const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
