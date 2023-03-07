133535e42db02054cd601bf3d7c48a78ebbc132a
Ilya Rodin <i1.rodin@yadnex.ru>
2023-03-07 19:57:40 +0300

    modify generate number function

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
-export default (maxNumber = 15) => Math.floor(Math.random() * maxNumber + 1);
+export default (maxNumber = 15, count = 1) => {
+  if (count < 1) {
+    console.log(`count cannot be less than 1`);
+    return;
+  }
+
+  if (count > 1) {
+    const result = [];
+    for (let i = 0; i < count; i += 1) {
+      result.push(Math.floor(Math.random() * maxNumber + 1));
+    }
+    return result;
+  }
+  return Math.floor(Math.random() * maxNumber + 1);
+};
