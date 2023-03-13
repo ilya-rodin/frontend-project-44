96eb62fd12640ac6ef09ca220b245ca1202b919e
Ilya Rodin <i1.rodin@yadnex.ru>
2023-03-13 23:33:24 +0300

    remove unnecessary condition

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const generateNumber = (min, max) => {
-  if (min < 0) {
-    console.log('count cannot be less than 0');
-    return null;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+export const generateNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
